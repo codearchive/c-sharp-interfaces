9bae2a402d7467eff42b9aa90ceb37739be8bee0
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-24 23:00:49 +0300

    Completed unit 7.4

---
+++ module_07/PeopleViewer.Presentation.Tests/FakeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace PeopleViewer.Presentation.Tests
+{
+    public class FakeRepository : IPersonRepository
+    {
+        public IEnumerable<Person> GetPeople()
+        {
+            return TestData.testPeople;
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- module_07/PeopleViewer.Presentation.Tests/PeopleViewModelTests.cs
+++ module_07/PeopleViewer.Presentation.Tests/PeopleViewModelTests.cs
@@ -9,30 +9,40 @@ namespace PeopleViewer.Presentation.Tests
 {
     public class PeopleViewModelTests
     {
+        private IPersonRepository GetTestRepository()
+        {
+            return new FakeRepository();
+        }
         [Test]
         public void RefreshPeople_OnExecute_PeopleIsPopulated()
         {
             // Arrange
+            var repository = new FakeRepository();
+            var viewModel = new PeopleViewModel(repository);
 
             // Act
+            viewModel.RefreshPeople();
 
             // Assert
+            Assert.IsNotNull(viewModel.People);
+            Assert.AreEqual(2, viewModel.People.Count());
+
         }
 
-        //[Test]
-        //public void ClearPeople_OnExecute_PeopleIsEmpty()
-        //{
-        //    // Arrange
-        //    var repository = GetTestRepository();
-        //    var vm = new PeopleViewModel(repository);
-        //    vm.RefreshPeople();
-        //    Assert.AreEqual(2, vm.People.Count(), "Invalid Arrangement");
+        [Test]
+        public void ClearPeople_OnExecute_PeopleIsEmpty()
+        {
+            // Arrange
+            var repository = GetTestRepository();
+            var vm = new PeopleViewModel(repository);
+            vm.RefreshPeople();
+            Assert.AreEqual(2, vm.People.Count(), "Invalid Arrangement");
 
-        //    // Act
-        //    vm.ClearPeople();
+            // Act
+            vm.ClearPeople();
 
-        //    // Assert
-        //    Assert.AreEqual(0, vm.People.Count());
-        //}
+            // Assert
+            Assert.AreEqual(0, vm.People.Count());
+        }
     }
 }
